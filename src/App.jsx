0b6898e9e88533e2e70cc01ddbed48accce38661
PalitBabin <palitsourav212914@gmail.com>
2023-09-12 22:46:10 +0530

    Reducing the initial bundle size

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
 import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
-import Home from "./pages/home/Home";
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-import UserList from "./pages/userList/UserList";
-import User from "./pages/user/User";
-import NewUser from "./pages/newUser/NewUser";
-import ProductList from "./pages/productList/ProductList";
-import Product from "./pages/product/Product";
-import NewProduct from "./pages/newProduct/NewProduct";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import('./pages/home/Home'));
+const UserList = lazy(() => import("./pages/userList/UserList"));
+const User = lazy(() => import("./pages/user/User"));
+const NewUser = lazy(() => import("./pages/newUser/NewUser"));
+const ProductList = lazy(() => import("./pages/productList/ProductList"));
+const Product = lazy(() => import("./pages/product/Product"));
+const NewProduct = lazy(() => import("./pages/newProduct/NewProduct"));
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
       <div className="container">
-      <Sidebar />
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/users" element={<UserList />} />
-      <Route path="/users/:id" element={<User/>} />
-      <Route path="/newUser" element={<NewUser/>} />
-      <Route path="/products" element={<ProductList/>} />
-      <Route path="/product/:productId" element={<Product/>} />
-      <Route path="/newProduct" element={<NewProduct/>} />
-      </Routes>
+        <Sidebar />
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<UserList />} />
+            <Route path="/users/:id" element={<User />} />
+            <Route path="/newUser" element={<NewUser />} />
+            <Route path="/products" element={<ProductList />} />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/newProduct" element={<NewProduct />} />
+          </Routes>
+        </Suspense>
       </div>
     </BrowserRouter>
   )
